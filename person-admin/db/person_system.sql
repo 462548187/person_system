27db1439c43123a702a713c7cb03006b8402499b
598187998 <598187998@qq.com>
2020-05-10 22:49:44 +0800

    更新数据sql及数据模型

--- person-admin/db/person_system.sql
+++ person-admin/db/person_system.sql
@@ -11,11 +11,188 @@
  Target Server Version : 50548
  File Encoding         : 65001
 
- Date: 10/05/2020 15:37:34
+ Date: 10/05/2020 22:48:06
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for b_convert_apply
+-- ----------------------------
+DROP TABLE IF EXISTS `b_convert_apply`;
+CREATE TABLE `b_convert_apply`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `apply_user_id` bigint(20) NULL DEFAULT NULL COMMENT '申请人',
+  `apply_date` varchar(24) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '申请日期',
+  `status` int(11) NULL DEFAULT 0 COMMENT '状态',
+  `approval_date` varchar(24) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '审批日期',
+  `approval_user_id` varchar(24) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '审批人',
+  `approval_result` int(11) NULL DEFAULT NULL COMMENT '审批结果',
+  `approval_opinion` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '审批意见',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '转正申请' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of b_convert_apply
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for b_interview_plan
+-- ----------------------------
+DROP TABLE IF EXISTS `b_interview_plan`;
+CREATE TABLE `b_interview_plan`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `candidate` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '候选人姓名',
+  `candidate_mobile` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '候选人电话',
+  `meet_time` varchar(24) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '面试时间',
+  `meet_user_id` bigint(20) NULL DEFAULT NULL COMMENT '面试官',
+  `status` int(11) NULL DEFAULT 0 COMMENT '面试结果',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '面试计划' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of b_interview_plan
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for b_recruit_need
+-- ----------------------------
+DROP TABLE IF EXISTS `b_recruit_need`;
+CREATE TABLE `b_recruit_need`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `dept` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '需求部门',
+  `need_num` int(11) NULL DEFAULT NULL COMMENT '需求人数',
+  `post` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '岗位类型',
+  `education` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学历要求',
+  `work_time` int(11) NULL DEFAULT NULL COMMENT '工作年限要求',
+  `demand` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '招聘要求',
+  `status` int(11) NULL DEFAULT NULL COMMENT '需求状态',
+  `recruit_user_id` bigint(20) NULL DEFAULT NULL COMMENT '招聘负责人',
+  `need_user_id` bigint(20) NULL DEFAULT NULL COMMENT '需求负责人',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '招聘需求' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of b_recruit_need
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for b_salary_record
+-- ----------------------------
+DROP TABLE IF EXISTS `b_salary_record`;
+CREATE TABLE `b_salary_record`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '用户id',
+  `salary_month` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '工资月份',
+  `must_salary` float NULL DEFAULT NULL COMMENT '应发工资',
+  `reality_salary` float NULL DEFAULT NULL COMMENT '实发工资',
+  `fund_amount` float NULL DEFAULT NULL COMMENT '公积金扣款',
+  `tax_amount` float NULL DEFAULT NULL COMMENT '个税扣款',
+  `medical_amount` float NULL DEFAULT NULL COMMENT '医保扣款',
+  `pension_amount` float NULL DEFAULT NULL COMMENT '养老扣款',
+  `injured_amount` float NULL DEFAULT NULL COMMENT '工伤扣款',
+  `birth_amount` float NULL DEFAULT NULL COMMENT '生育扣款',
+  `unemployment_amount` float NULL DEFAULT NULL COMMENT '失业扣款',
+  `leave_amount` float NULL DEFAULT NULL COMMENT '请假扣款',
+  `late_amount` float NULL DEFAULT NULL COMMENT '迟到扣款',
+  `base_amount` float NULL DEFAULT NULL COMMENT '基本工资',
+  `work_amount` float NULL DEFAULT NULL COMMENT '工龄工资',
+  `overtime_amount` float NULL DEFAULT NULL COMMENT '加班费',
+  `merits_amount` float NULL DEFAULT NULL COMMENT '绩效奖金',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '工资记录' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of b_salary_record
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for b_user_doc
+-- ----------------------------
+DROP TABLE IF EXISTS `b_user_doc`;
+CREATE TABLE `b_user_doc`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '员工id',
+  `education` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学历',
+  `birth` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '生日',
+  `entry_date` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '入职日期',
+  `user_type` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '员工类型',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '员工档案' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of b_user_doc
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for b_user_plan
+-- ----------------------------
+DROP TABLE IF EXISTS `b_user_plan`;
+CREATE TABLE `b_user_plan`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '员工',
+  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '计划名称',
+  `start_date` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '开始日期',
+  `end_date` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '结束日期',
+  `status` int(11) NULL DEFAULT 0 COMMENT '计划状态',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '个人计划' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of b_user_plan
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for b_work_daily
+-- ----------------------------
+DROP TABLE IF EXISTS `b_work_daily`;
+CREATE TABLE `b_work_daily`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '员工id',
+  `work_date` varchar(24) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '工作日期',
+  `work_content` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '工作内容',
+  `progress` int(11) NULL DEFAULT NULL COMMENT '完成进度',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '工作日报' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of b_work_daily
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for b_work_record
+-- ----------------------------
+DROP TABLE IF EXISTS `b_work_record`;
+CREATE TABLE `b_work_record`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '员工id',
+  `work_date` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '工作日期',
+  `up_time` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '上班时间',
+  `down_time` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '下班时间',
+  `status` int(11) NULL DEFAULT NULL COMMENT '状态',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '出勤记录' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of b_work_record
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for sys_config
@@ -96,13 +273,29 @@ CREATE TABLE `sys_log`  (
   `ip` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'IP地址',
   `create_date` datetime NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统日志' ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统日志' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of sys_log
 -- ----------------------------
 INSERT INTO `sys_log` VALUES (1, 'admin', '删除菜单', 'io.renren.modules.sys.controller.SysMenuController.delete()', '30', 0, '0:0:0:0:0:0:0:1', '2020-05-10 13:16:24');
 INSERT INTO `sys_log` VALUES (2, 'admin', '删除菜单', 'io.renren.modules.sys.controller.SysMenuController.delete()', '14', 0, '0:0:0:0:0:0:0:1', '2020-05-10 13:18:22');
+INSERT INTO `sys_log` VALUES (3, 'admin', '修改菜单', 'com.person.modules.sys.controller.SysMenuController.update()', '{\"menuId\":2,\"parentId\":1,\"parentName\":\"系统管理\",\"name\":\"员工管理\",\"url\":\"modules/sys/user.html\",\"type\":1,\"icon\":\"fa fa-user\",\"orderNum\":1}', 24, '0:0:0:0:0:0:0:1', '2020-05-10 15:43:45');
+INSERT INTO `sys_log` VALUES (4, 'admin', '保存菜单', 'com.person.modules.sys.controller.SysMenuController.save()', '{\"menuId\":41,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"工资管理\",\"type\":0,\"icon\":\"cny\",\"orderNum\":0}', 17, '0:0:0:0:0:0:0:1', '2020-05-10 18:04:39');
+INSERT INTO `sys_log` VALUES (5, 'admin', '修改菜单', 'com.person.modules.sys.controller.SysMenuController.update()', '{\"menuId\":41,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"工资管理\",\"type\":0,\"icon\":\"http://www.fontawesome.com.cn/faicons/\",\"orderNum\":0}', 16, '0:0:0:0:0:0:0:1', '2020-05-10 18:05:31');
+INSERT INTO `sys_log` VALUES (6, 'admin', '修改菜单', 'com.person.modules.sys.controller.SysMenuController.update()', '{\"menuId\":41,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"工资管理\",\"type\":0,\"icon\":\"fa fa-jpy\",\"orderNum\":0}', 7, '0:0:0:0:0:0:0:1', '2020-05-10 18:05:51');
+INSERT INTO `sys_log` VALUES (7, 'admin', '修改菜单', 'com.person.modules.sys.controller.SysMenuController.update()', '{\"menuId\":41,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"工资管理\",\"type\":0,\"icon\":\"fa fa-jpy\",\"orderNum\":1}', 6, '0:0:0:0:0:0:0:1', '2020-05-10 18:06:06');
+INSERT INTO `sys_log` VALUES (8, 'admin', '修改菜单', 'com.person.modules.sys.controller.SysMenuController.update()', '{\"menuId\":1,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"系统管理\",\"type\":0,\"icon\":\"fa fa-cog\",\"orderNum\":10}', 7, '0:0:0:0:0:0:0:1', '2020-05-10 18:06:53');
+INSERT INTO `sys_log` VALUES (9, 'admin', '保存菜单', 'com.person.modules.sys.controller.SysMenuController.save()', '{\"menuId\":42,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"考勤管理\",\"type\":0,\"icon\":\"fa fa-calendar-check-o\",\"orderNum\":2}', 7, '0:0:0:0:0:0:0:1', '2020-05-10 18:09:23');
+INSERT INTO `sys_log` VALUES (10, 'admin', '保存菜单', 'com.person.modules.sys.controller.SysMenuController.save()', '{\"menuId\":43,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"个人中心\",\"type\":0,\"orderNum\":3}', 20, '0:0:0:0:0:0:0:1', '2020-05-10 21:52:11');
+INSERT INTO `sys_log` VALUES (11, 'admin', '保存菜单', 'com.person.modules.sys.controller.SysMenuController.save()', '{\"menuId\":44,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"招聘管理\",\"type\":0,\"orderNum\":4}', 8, '0:0:0:0:0:0:0:1', '2020-05-10 21:52:36');
+INSERT INTO `sys_log` VALUES (12, 'admin', '保存菜单', 'com.person.modules.sys.controller.SysMenuController.save()', '{\"menuId\":45,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"档案管理\",\"type\":0,\"icon\":\"\",\"orderNum\":4}', 8, '0:0:0:0:0:0:0:1', '2020-05-10 21:53:19');
+INSERT INTO `sys_log` VALUES (13, 'admin', '修改菜单', 'com.person.modules.sys.controller.SysMenuController.update()', '{\"menuId\":45,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"档案管理\",\"type\":0,\"icon\":\"\",\"orderNum\":5}', 14, '0:0:0:0:0:0:0:1', '2020-05-10 21:53:28');
+INSERT INTO `sys_log` VALUES (14, 'admin', '修改菜单', 'com.person.modules.sys.controller.SysMenuController.update()', '{\"menuId\":44,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"招聘管理\",\"type\":0,\"orderNum\":1}', 6, '0:0:0:0:0:0:0:1', '2020-05-10 21:53:52');
+INSERT INTO `sys_log` VALUES (15, 'admin', '修改菜单', 'com.person.modules.sys.controller.SysMenuController.update()', '{\"menuId\":45,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"档案管理\",\"type\":0,\"icon\":\"\",\"orderNum\":2}', 6, '0:0:0:0:0:0:0:1', '2020-05-10 21:54:12');
+INSERT INTO `sys_log` VALUES (16, 'admin', '修改菜单', 'com.person.modules.sys.controller.SysMenuController.update()', '{\"menuId\":41,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"工资管理\",\"type\":0,\"icon\":\"fa fa-jpy\",\"orderNum\":3}', 7, '0:0:0:0:0:0:0:1', '2020-05-10 21:54:34');
+INSERT INTO `sys_log` VALUES (17, 'admin', '修改菜单', 'com.person.modules.sys.controller.SysMenuController.update()', '{\"menuId\":42,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"考勤管理\",\"type\":0,\"icon\":\"fa fa-calendar-check-o\",\"orderNum\":4}', 6, '0:0:0:0:0:0:0:1', '2020-05-10 21:54:41');
+INSERT INTO `sys_log` VALUES (18, 'admin', '修改菜单', 'com.person.modules.sys.controller.SysMenuController.update()', '{\"menuId\":43,\"parentId\":0,\"parentName\":\"一级菜单\",\"name\":\"个人中心\",\"type\":0,\"orderNum\":5}', 6, '0:0:0:0:0:0:0:1', '2020-05-10 21:54:50');
 
 -- ----------------------------
 -- Table structure for sys_menu
@@ -118,13 +311,13 @@ CREATE TABLE `sys_menu`  (
   `icon` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单图标',
   `order_num` int(11) NULL DEFAULT NULL COMMENT '排序',
   PRIMARY KEY (`menu_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 41 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '菜单管理' ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 46 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '菜单管理' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of sys_menu
 -- ----------------------------
-INSERT INTO `sys_menu` VALUES (1, 0, '系统管理', NULL, NULL, 0, 'fa fa-cog', 0);
-INSERT INTO `sys_menu` VALUES (2, 1, '用户管理', 'modules/sys/user.html', NULL, 1, 'fa fa-user', 1);
+INSERT INTO `sys_menu` VALUES (1, 0, '系统管理', NULL, NULL, 0, 'fa fa-cog', 10);
+INSERT INTO `sys_menu` VALUES (2, 1, '员工管理', 'modules/sys/user.html', NULL, 1, 'fa fa-user', 1);
 INSERT INTO `sys_menu` VALUES (3, 1, '角色管理', 'modules/sys/role.html', NULL, 1, 'fa fa-user-secret', 2);
 INSERT INTO `sys_menu` VALUES (4, 1, '菜单管理', 'modules/sys/menu.html', NULL, 1, 'fa fa-th-list', 3);
 INSERT INTO `sys_menu` VALUES (15, 2, '查看', NULL, 'sys:user:list,sys:user:info', 2, NULL, 0);
@@ -149,6 +342,11 @@ INSERT INTO `sys_menu` VALUES (37, 36, '查看', NULL, 'sys:dict:list,sys:dict:i
 INSERT INTO `sys_menu` VALUES (38, 36, '新增', NULL, 'sys:dict:save', 2, NULL, 6);
 INSERT INTO `sys_menu` VALUES (39, 36, '修改', NULL, 'sys:dict:update', 2, NULL, 6);
 INSERT INTO `sys_menu` VALUES (40, 36, '删除', NULL, 'sys:dict:delete', 2, NULL, 6);
+INSERT INTO `sys_menu` VALUES (41, 0, '工资管理', NULL, NULL, 0, 'fa fa-jpy', 3);
+INSERT INTO `sys_menu` VALUES (42, 0, '考勤管理', NULL, NULL, 0, 'fa fa-calendar-check-o', 4);
+INSERT INTO `sys_menu` VALUES (43, 0, '个人中心', NULL, NULL, 0, NULL, 5);
+INSERT INTO `sys_menu` VALUES (44, 0, '招聘管理', NULL, NULL, 0, NULL, 1);
+INSERT INTO `sys_menu` VALUES (45, 0, '档案管理', NULL, NULL, 0, '', 2);
 
 -- ----------------------------
 -- Table structure for sys_oss
@@ -226,6 +424,7 @@ CREATE TABLE `sys_user`  (
   `status` tinyint(4) NULL DEFAULT NULL COMMENT '状态  0：禁用   1：正常',
   `dept_id` bigint(20) NULL DEFAULT NULL COMMENT '部门ID',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`user_id`) USING BTREE,
   UNIQUE INDEX `username`(`username`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统用户' ROW_FORMAT = Compact;
@@ -233,7 +432,7 @@ CREATE TABLE `sys_user`  (
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES (1, 'admin', 'e1153123d7d180ceeb820d577ff119876678732a68eef4e6ffc0b1f06a01f91b', 'YzcmCZNvbXocrsz9dm8e', 'root@renren.io', '13612345678', 1, 1, '2016-11-11 11:11:11');
+INSERT INTO `sys_user` VALUES (1, 'admin', 'e1153123d7d180ceeb820d577ff119876678732a68eef4e6ffc0b1f06a01f91b', 'YzcmCZNvbXocrsz9dm8e', 'root@renren.io', '13612345678', 1, 1, '2016-11-11 11:11:11', '管理员');
 
 -- ----------------------------
 -- Table structure for sys_user_role
